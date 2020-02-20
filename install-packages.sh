61acb4c01f5fbc3adb5c9ff79c2ad348c1e43cd4
Steven Armstrong <steven.armstrong@id.ethz.ch>
2020-02-20 15:35:01 +0100

    initial commit
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

---
+++ install-packages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Tell apt-get we're never going to be able to give manual feedback:
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the package listing, so we know what packages exist:
+apt-get update
+
+# Install security updates:
+apt-get -y upgrade
+
+# Install our dependencies without unnecessary recommended packages:
+apt-get -y install --no-install-recommends \
+   openssh-client git curl rsync openssl ca-certificates
+
+update-ca-certificates
+
+# Delete cached files we don't need anymore:
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
